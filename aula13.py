a02dcdebc002de57bc5a3858927f5f3dd34a58e2
Ruan Rodrigues <ruan.arjoaquim@outlook.com>
2025-09-06 23:48:10 -0300

    Utilizando F strings

---
+++ aula13.py
@@ -0,0 +1,16 @@
+nome = 'Ruan Rodrigues'
+altura = 1.79
+peso = 70
+imc = float(peso / altura ** 2)
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Ruan Rodrigues tem 1.79 de altura,
+# pesa 70 quilos e seu imc é        
+# 21.847008520333322
