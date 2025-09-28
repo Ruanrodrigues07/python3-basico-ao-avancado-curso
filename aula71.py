b333d8b80d80cfef4fe12202b9f9041e3e0e6e9b
Ruan Rodrigues <ruan.arjoaquim@outlook.com>
2025-09-28 18:58:50 -0300

    args - Argumentos não nomeados

--- aula71.py
+++ aula71.py
@@ -3,7 +3,7 @@ args - Argumentos não nomeados
 * - *args (empacotamento e desempacotamento)
 """
 # Lembre-te de desempacotamento
-# x, y, *resto = 1, 2, 3, 4
+# x, y, *resto = (1, 2, 3, 4)
 # print(x, y, resto)
 
 
